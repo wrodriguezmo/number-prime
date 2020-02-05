2fdc5f02b249f53a17cb9483f3464f7b38cb9f79
William Rodríguez <wrodriguezmo@gmail.com>
2020-02-05 03:03:10 -0500

    Version original class 04Feb

---
+++ a.js
@@ -0,0 +1,18 @@
+function numberPrime(until) {
+    var number = [];
+    for (var i = 2; i < until; i++) {
+        primo = true;
+        for (var count = 2; count < i; i++) {
+            if (i % count == 0) {
+                primo = false;
+                break;
+            }
+            debugger    
+        } 
+        console.log(primo)
+        if (primo) {
+            console.log(i)
+            number.push(i)
+        }
+    }
+}
