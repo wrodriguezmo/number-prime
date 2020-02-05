79f6b2c2b2032953920ebc2fecb0f50386c06d92
William Rodríguez <wrodriguezmo@gmail.com>
2020-02-05 03:07:00 -0500

    Fix error

--- a.js
+++ a.js
@@ -2,7 +2,7 @@ function numberPrime(until) {
     var number = [];
     for (var i = 2; i < until; i++) {
         primo = true;
-        for (var count = 2; count < i; i++) {
+        for (var count = 2; count < i; count++) {
             if (i % count == 0) {
                 primo = false;
                 break;
