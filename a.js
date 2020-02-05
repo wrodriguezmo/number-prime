487b4817174d0c95c0069783e7263016d187bccc
William Rodríguez <wrodriguezmo@gmail.com>
2020-02-05 03:50:44 -0500

    without brackets

--- a.js
+++ a.js
@@ -2,15 +2,13 @@
     var number = [2]
     for (let i=3; i<until; i++){
         prime = true;
-        for (let count=2; count<i; count++){
+        for (let count=2; count<i; count++)
             if (i%count == 0){
                 prime = false;
                 break;
             }
-        } 
-        if (prime) {
+        if (prime)
             number.push(i)
-        }
     }
     console.log(number);
 })(100)
